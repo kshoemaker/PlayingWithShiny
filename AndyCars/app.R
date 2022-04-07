36a91ebf54cf4c2bc4bee76d01d40c70528dc91d
Kate Shoemaker <katherine.shoemaker@gmail.com>
2022-04-07 16:34:22 -0500

    figured out the table

--- AndyCars/app.R
+++ AndyCars/app.R
@@ -11,17 +11,17 @@ library(shiny)
 library(tidyverse)
 
 my_cars <- rownames_to_column(mtcars, var = "cars")
-car_choices <- mtcars$cars
+car_choices <- my_cars$cars
 # Define UI for application that draws a histogram
 ui <- fluidPage(
 
     # Application title
-    titlePanel("Old Faithful Geyser Data"),
+    titlePanel("Car Info"),
 
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
         sidebarPanel(
-            selectInput("cars",
+            selectInput("chosen_cars",
                         label = "What cars are you interested in?",
                         choices = car_choices,
                         multiple = T )
@@ -37,7 +37,10 @@ ui <- fluidPage(
 # Define server logic required to draw a histogram
 server <- function(input, output) {
     
-    output$car_info <- 
+    output$car_info <- renderTable({
+        table <- my_cars %>% filter(cars %in% input$chosen_cars) %>% select(cars, mpg, hp, qsec)
+        table
+    })
     
 }
 
